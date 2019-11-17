637dd6f9c34828443c908bcaec95303b507c8748
Slimane Salim <shahra.admogmail.com>
2019-11-17 05:35:24 +0100

    رفع العمل الخاص بالاخت دعاء

--- app/src/main/java/com/devimpact/startup/OurServicesActivity.java
+++ app/src/main/java/com/devimpact/startup/OurServicesActivity.java
@@ -1,58 +1,101 @@
 package com.devimpact.startup;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.drawerlayout.widget.DrawerLayout;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.navigation.NavigationView;
+//import com.google.firebase.auth.FirebaseAuth;
+import java.io.File;
+
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.devimpact.startup.R;
 
-import android.view.Menu;
-
-public class OurServicesActivity extends AppCompatActivity {
-
-
-    private AppBarConfiguration mAppBarConfiguration;
+public class OurServicesActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+    FragmentTransaction fragmentTransaction;
+    FragmentManager fragmentManager;
+    //Add Navigation Drawer
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_our_services);
+        //Add Navigation Drawer
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_about, R.id.nav_Team_Members,
-                R.id.nav_share, R.id.nav_rate)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(OurServicesActivity.this);
+
+
+
     }
-
-
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        final String appUrl = "https://play.google.com/store/apps/details?id=com.devimpact.startup";
+
+        if (id == R.id.nav_home) {
+
+            Intent it = new Intent(OurServicesActivity.this, OurServicesActivity.class);
+            startActivity(it);
+        } else if (id == R.id.nav_about) {
+            Intent it = new Intent(OurServicesActivity.this, com.devimpact.startup.activities.about.About.class);
+            startActivity(it);
+        }  else if (id == R.id.nav_share) {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, appUrl);
+            startActivity(Intent.createChooser(shareIntent, "Share using"));
+
+        } else if (id == R.id.nav_rate) {
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(OurServicesActivity.this);
+            dialogBuilder.setTitle(R.string.rate_s);
+            dialogBuilder.setMessage(R.string.if_you);
+            dialogBuilder.setPositiveButton(R.string.rate_s, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(appUrl));
+                    startActivity(i);
+                    dialog.dismiss();
+                }
+            });
+            AlertDialog dialog = dialogBuilder.create();
+            dialog.show();
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
